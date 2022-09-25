fd30291eb904ad6cbf013bbaa951ef85980d1461
teawithsand <teawithsand@gmail.com>
2022-09-25 12:54:23 +0000

    Added getPrevSourceKey method

--- tws-player/src/source/provider.ts
+++ tws-player/src/source/provider.ts
@@ -8,18 +8,25 @@ import { SourcePlayerError } from "../error"
  */
 export interface PlayerSourceProvider<S, SK> {
 	getNextSourceKey(sk: SK | null): SK | null
+	getPrevSourceKey(sk: SK | null): SK | null
 	providerSourceWithKey(key: SK): S
 }
 
 export class ArrayPlayerSourceProvider<S>
 	implements PlayerSourceProvider<S, number>
 {
-	constructor(private readonly arr: S[]) {}
+	constructor(public readonly arr: S[]) {}
 
 	getNextSourceKey = (sk: number | null): number | null => {
 		sk = sk ?? 0
 		if (sk >= this.arr.length) return null
 		return sk + 1
+	}
+
+	getPrevSourceKey = (sk: number | null): number | null => {
+		sk = sk ?? 0
+		if (sk === 0 || sk >= this.arr.length) return null
+		return sk - 1
 	}
 
 	providerSourceWithKey = (i: number): S => {
