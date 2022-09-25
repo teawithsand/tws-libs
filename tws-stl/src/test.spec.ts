185040638e6eae393438d59b16d59466b754d8f0
teawithsand <teawithsand@gmail.com>
2022-09-25 07:19:09 +0000

    Iniitial commit

---
+++ tws-stl/src/test.spec.ts
@@ -0,0 +1,9 @@
+import { hellWorld } from "."
+
+describe("hell world", () => {
+	{
+		it("can be called in order to acknowledge that the world is indeed hell", () => {
+			hellWorld()
+		})
+	}
+})
