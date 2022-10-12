0854dad84c85cb9fe9c8cf093e7729568030cd5c
teawithsand <teawithsand@gmail.com>
2022-10-12 08:17:22 +0000

    Added util for reading constant size numbers

--- app-pk2-web/src/util/binary/reader.ts
+++ app-pk2-web/src/util/binary/reader.ts
@@ -48,6 +48,45 @@ export class BinaryReader {
 		return res
 	}
 
+	readNByteNumber = (sz: 1 | 2 | 4 | 8, unsigned = true): number => {
+		const movedBuffer = this.buffer.slice(this.offset)
+		let isError = false
+		try {
+			if (unsigned) {
+				if (sz === 1) {
+					return new Uint8Array(movedBuffer)[0]
+				} else if (sz === 2) {
+					return new Uint16Array(movedBuffer)[0]
+				} else if (sz === 4) {
+					return new Uint32Array(movedBuffer)[0]
+				} else if (sz === 8) {
+					return Number(new BigUint64Array(movedBuffer)[0])
+				} else {
+					throw new Error(`Invalid unsigned number size ${sz}`)
+				}
+			} else {
+				if (sz === 1) {
+					return new Int8Array(movedBuffer)[0]
+				} else if (sz === 2) {
+					return new Int16Array(movedBuffer)[0]
+				} else if (sz === 4) {
+					return new Int32Array(movedBuffer)[0]
+				} else if (sz === 8) {
+					return Number(new BigInt64Array(movedBuffer)[0])
+				} else {
+					throw new Error(`Invalid signed number size ${sz}`)
+				}
+			}
+		} catch (e) {
+			isError = true
+			throw e // TODO(teawithsand): refactor me
+		} finally {
+			if (!isError) {
+				this.offset += sz
+			}
+		}
+	}
+
 	advance = (n: number) => {
 		this.offset += n
 	}
