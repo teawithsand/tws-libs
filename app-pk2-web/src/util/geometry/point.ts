d89168e35319de37b7663514f157e63882bb4f1b
teawithsand <teawithsand@gmail.com>
2022-10-17 10:15:37 +0000

    Ported geometry util

---
+++ app-pk2-web/src/util/geometry/point.ts
@@ -0,0 +1,110 @@
+export type PointLike =
+	| [number, number]
+	| {
+			x: number
+			y: number
+	  }
+	| Point
+
+export class Point {
+	public readonly x: number
+	public readonly y: number
+
+	get 0() {
+		return this.x
+	}
+	get 1() {
+		return this.y
+	}
+	constructor(p?: PointLike) {
+		p = p ?? [0, 0]
+		if (p instanceof Array) {
+			this.x = p[0]
+			this.y = p[1]
+		} else {
+			this.x = p.x
+			this.y = p.y
+		}
+	}
+
+	equals(p: Point) {
+		return this.x === p.x && this.y === p.y
+	}
+
+	toJSON() {
+		return this.toArray()
+	}
+
+	toArray(): [number, number] {
+		return [this.x, this.y]
+	}
+
+	translated = (...points: Point[]) => {
+		return new Point(
+			[this, ...points].reduce(
+				(prev, current) =>
+					new Point([prev.x + current.x, prev.y + current.y])
+			)
+		)
+	}
+
+	euclideanDistance = (...points: Point[]): number => {
+		let d = 0
+
+		let prevPoint: Point = this
+		for (const p of points) {
+			d += Math.sqrt((prevPoint.x - p.x) ** 2 + (prevPoint.y - p.y) ** 2)
+			prevPoint = p
+		}
+
+		return d
+	}
+
+	manhattanDistance = (...points: Point[]): number => {
+		let d = 0
+
+		let prevPoint: Point = this
+		for (const p of points) {
+			d += Math.abs(prevPoint.x - p.x) + Math.abs(prevPoint.y - p.y)
+			prevPoint = p
+		}
+
+		return d
+	}
+
+	/**
+	 * Returns distance of this point from segment defined by these two points.
+	 */
+	segmentDistance = (segment: [Point, Point]): number => {
+		const [x, y] = this.toArray()
+		const [[x1, y1], [x2, y2]] = segment.map((p) => p.toArray())
+
+		const A = x - x1
+		const B = y - y1
+		const C = x2 - x1
+		const D = y2 - y1
+
+		const dot = A * C + B * D
+		const len_sq = C * C + D * D
+		let param = -1
+		if (len_sq != 0)
+			//in case of 0 length line
+			param = dot / len_sq
+
+		let xx, yy
+		if (param < 0) {
+			xx = x1
+			yy = y1
+		} else if (param > 1) {
+			xx = x2
+			yy = y2
+		} else {
+			xx = x1 + param * C
+			yy = y1 + param * D
+		}
+
+		const dx = x - xx
+		const dy = y - yy
+		return Math.sqrt(dx ** 2 + dy ** 2)
+	}
+}
---
+++ app-pk2-web/src/util/geometry/rect.ts
@@ -0,0 +1,160 @@
+import { Point, PointLike } from "./point"
+
+export type RectLike =
+	| [number, number, number, number]
+	| [PointLike, PointLike]
+	| Rect
+	| Point
+
+export class Rect {
+	public readonly p1: Point
+	public readonly p2: Point
+
+	constructor(r?: RectLike) {
+		r = r ?? new Point([0, 0])
+		if (r instanceof Rect) {
+			this.p1 = r.p1
+			this.p2 = r.p2
+			return
+		} else if (r instanceof Point) {
+			this.p1 = r
+			this.p2 = r
+			return
+		}
+
+		if (r.length === 4) {
+			this.p1 = new Point([r[0], r[1]])
+			this.p2 = new Point([r[2], r[3]])
+		} else if (r.length === 2) {
+			this.p1 = new Point(r[0])
+			this.p2 = new Point(r[1])
+		} else {
+			throw new Error("Invalid rect-like data")
+		}
+	}
+
+	toArray(): [number, number, number, number] {
+		return [this.p1.x, this.p1.y, this.p2.x, this.p2.y]
+	}
+	toPoints(): [PointLike, PointLike] {
+		return [this.p1, this.p2]
+	}
+
+	toJSON() {
+		return [this.p1.toJSON(), this.p2.toJSON()]
+	}
+
+	equals(p: Rect) {
+		return this.p1.equals(p.p1) && this.p2.equals(p.p2)
+	}
+
+	get normalized(): Rect {
+		const [x1, y1, x2, y2] = this.toArray()
+
+		return new Rect([
+			[Math.min(x1, x2), Math.min(y1, y2)],
+			[Math.max(x1, x2), Math.max(y1, y2)],
+		])
+	}
+	get width(): number {
+		return Math.abs(this.p1.x - this.p2.y)
+	}
+
+	get height(): number {
+		return Math.abs(this.p1.y - this.p2.y)
+	}
+
+	get area(): number {
+		return this.width * this.height
+	}
+
+	translated(p: Point) {
+		return new Rect([this.p1.translated(p), this.p2.translated(p)])
+	}
+
+	grow(by: number) {
+		const [x1, y1, x2, y2] = this.normalized.toArray()
+		return new Rect([x1 - by, y1 - by, x2 + by, y2 + by])
+	}
+
+	containsPoint(p: Point, bordersAllowed = true) {
+		const rect = this.normalized
+
+		if (
+			bordersAllowed &&
+			rect.p1.x >= p.x &&
+			rect.p2.y >= p.y &&
+			rect.p1.x <= p.x &&
+			rect.p2.y <= p.y
+		) {
+			return true
+		} else if (
+			rect.p1.x > p.x &&
+			rect.p2.y > p.y &&
+			rect.p1.x < p.x &&
+			rect.p2.y < p.y
+		) {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	intersection(...rects: Rect[]): Rect | null {
+		let current = this.normalized
+		for (let rect of rects) {
+			rect = rect.normalized
+
+			if (
+				!rect.containsPoint(current.p1) &&
+				!rect.containsPoint(current.p2)
+			) {
+				return null // no intersection
+			}
+
+			let x1 = Math.max(rect.p1.x, current.p1.x)
+			let x2 = Math.min(rect.p2.x, current.p2.x)
+			;[x1, x2] = [Math.min(x1, x2), Math.max(x1, x2)]
+			let y1 = Math.max(rect.p1.y, current.p1.y)
+			let y2 = Math.min(rect.p2.y, current.p2.y)
+			;[y1, y2] = [Math.min(y1, y2), Math.max(y1, y2)]
+
+			current = new Rect([
+				[x1, y1],
+				[x2, y2],
+			]).normalized
+		}
+
+		return current
+	}
+
+	/**
+	 * Computes offsets, which once they are added to rectangle edges,
+	 * transform this rectangle into destination rectangle.
+	 */
+	getTransformOffsets(destRect: Rect) {
+		destRect = destRect.normalized
+		const norm = this.normalized
+
+		return {
+			left: destRect.p1.x - norm.p1.x,
+			right: destRect.p2.x - norm.p2.x,
+			bottom: destRect.p1.y - norm.p1.y,
+			top: destRect.p2.y - norm.p2.y,
+		}
+	}
+
+	/**
+	 * Just like getTransformOffsets, but:
+	 * - returns positive number when source rectangle, once applied transformation, will have given edge's length increased by that number
+	 * - returns negative number when source rectangle, once applied transformation, will have given edge's length decreased by that number
+	 */
+	getTransformDistances(destRect: Rect) {
+		const offsets = this.getTransformOffsets(destRect)
+		return {
+			...offsets,
+			left: -offsets.left,
+			bottom: -offsets.bottom,
+		}
+	}
+}
