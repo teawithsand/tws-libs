a54b3e042a4a97675976f0a5d9ed85ee42088d67
teawithsand <teawithsand@gmail.com>
2022-10-19 09:03:14 +0000

    Implemented first version of some map type definitions

--- app-pk2-web/src/game/map/defines.ts
+++ app-pk2-web/src/game/map/defines.ts
@@ -1,3 +1,5 @@
+export const BLOCK_WIDTH = 32
+export const BLOCK_HEIGHT = 32
 
 /**
  * Port of following enum:
@@ -10,7 +12,7 @@
  * const u8 ILMA_LUMISADE			= 4;
  * ```
  */
- export enum MapClimate {
+export enum MapClimate {
 	NORMAL = 0,
 	RAIN = 1,
 	FOREST = 2,
@@ -23,4 +25,49 @@ export enum MapBackgroundMovementType {
 	PARALLAX_VERTICAL = 1,
 	PARALLAX_HORIZONTAL = 2,
 	PARALLAX_VERTICAL_HORIZONTAL = 3,
+}
+
+export type BlockId = number
+
+export type BlockCollisionData = {
+	raw: boolean[][]
+	topRay: number[]
+	leftRay: number[]
+	rightRay: number[]
+	bottomRay: number[]
+}
+
+export type BlockData = {
+	foregroundURL: string // typically these are same
+	backgroundURL: string // typically these are same
+	collisions: BlockCollisionData // ignored when block is in background
+
+	// ignored for background blocks, in foreground blocks, used to determine whether entity is in water or not
+	isWater: boolean
+}
+
+/**
+ * Id of block that is not there
+ */
+export const emptyBlockId: BlockId = -1
+
+export type PrePlacedEntity = {
+	x: number
+	y: number
+	reference: string
+}
+
+export type MapData = {
+	blockData: {
+		[key: BlockId]: BlockData
+	}
+
+	backgroundImageURL: string
+
+	width: number
+	height: number
+
+	foregroundBlocks: BlockId[][]
+	backgroundBlocks: BlockId[][]
+	prePlacedEntities: PrePlacedEntity
 }
