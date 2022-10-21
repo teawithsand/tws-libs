2da2ab36e8f4d22adcffbb0a0c3ba979b74b75e4
teawithsand <teawithsand@gmail.com>
2022-10-21 05:44:49 +0000

    Refactored resource loader(NIY)

--- app-pk2-web/src/game/resources/gatsby.ts
+++ app-pk2-web/src/game/resources/gatsby.ts
@@ -1,51 +1,124 @@
-import { graphql, useStaticQuery } from "gatsby"
+import { throwExpression } from "@teawithsand/tws-stl"
 
-import { throwExpression } from "@app/../../tws-stl/dist"
-import { ExternalEntityData } from "@app/game/entity/external/data"
+import { parseLegacyEntityData } from "@app/game/entity/external/legacy"
+import {
+	MapData,
+	mapDataFromLegacy,
+	parseLegacyMap,
+	PrePlacedEntity,
+} from "@app/game/map"
+import { GameResources, LegacyResourceLoader } from "@app/game/resources/res"
 
-import { ResourceLoader } from "./res"
+export class GatsbyResourceLoader implements LegacyResourceLoader {
+	readonly legacyEpisodes: string[]
+	readonly legacyEpisodesMaps: {
+		[key: string]: string[]
+	}
 
-/**
- * Resource loader, which uses gatsby resources file source in order to provide all in-game assets.
- */
-export class GatsbyResourceLoader implements ResourceLoader {
-	private readonly blockMap: Map<number, string> = new Map()
-	constructor(
-		private readonly rawResources: Queries.ResourceFilesQuery,
-		tileSetName: string,
-		public readonly backgroundColor?: string | undefined,
-		public readonly backgroundImageUrl?: string | undefined,
-	) {
-		const m = this.blockMap // fixme
+	constructor(private readonly rawResources: Queries.ResourceFilesQuery) {
+		this.legacyEpisodes = [
+			...new Set(
+				rawResources.allFile.nodes
+					.map(
+						e =>
+							e.relativePath.match(
+								/^episodes\/(.*)\/.+\.map$/,
+							) ?? ["", ""],
+					)
+					.map(v => v[1])
+					.filter(v => !!v),
+			),
+		]
 
-		for (let i = 0; i < 255; i++) {
-			const res = rawResources.allFile.nodes.find(
-				v =>
-					v.relativePath ===
-					`blocks/${tileSetName}/${String(i).padStart(3, "0")}.png`,
+		this.legacyEpisodesMaps = Object.fromEntries(
+			this.legacyEpisodes.map(epName => [
+				epName,
+				rawResources.allFile.nodes
+					.map(
+						e =>
+							e.relativePath.match(
+								/^episodes\/(.*)\/(.+\.map)$/,
+							) ?? ["", "", ""],
+					)
+					.filter(v => v[1] === epName)
+					.map(v => v[2]),
+			]),
+		)
+	}
+
+	private get res() {
+		return this.rawResources.allFile.nodes
+	}
+
+	loadLegacyGameResources = async (
+		epName: string,
+		mapFileName: string,
+	): Promise<GameResources> => {
+		const mapFilePath = `episodes/${epName}/${mapFileName}`
+
+		const mapFileUrl =
+			(
+				this.res.find(p => p.relativePath === mapFilePath) ??
+				throwExpression(new Error("such map does not exist"))
+			).publicURL ??
+			throwExpression(
+				new Error(`Map file ${mapFilePath} does not have public URL`),
 			)
 
-			if (res && res.publicURL) {
-				m.set(i, res.publicURL)
-			}
+		const mapBuffer = await (
+			await (await fetch(mapFileUrl)).blob()
+		).arrayBuffer()
+
+		const legacyMapData = parseLegacyMap(mapBuffer)
+		const almostMapData = mapDataFromLegacy(legacyMapData)
+
+		const backgroundImageUrl =
+			this.res.find(
+				v =>
+					v.relativePath ===
+					`scenery/${legacyMapData.backgroundImagePath.replace(
+						".bmp",
+						".png",
+					)}`,
+			)?.publicURL ??
+			throwExpression(
+				new Error(
+					`Background image with path ${legacyMapData.backgroundImagePath} was not found`,
+				),
+			)
+
+		const prePlacedEntities: PrePlacedEntity[] = []
+		for (const e of almostMapData.prePlacedEntities) {
+			const url =
+				this.res.find(
+					v => v.relativePath === "models/data/" + e.reference,
+				)?.publicURL ??
+				throwExpression(
+					new Error(
+						`There is no entity file registered for ${e.reference}`,
+					),
+				)
+
+			// Let's just assume that HTTP cache is there and I do not have to cache it by hand
+			const buffer = await (await (await fetch(url)).blob()).arrayBuffer()
+			const legacyEntityData = parseLegacyEntityData(buffer)
+
+			throw new Error("NIY")
+			// TODO(teawithsand): port that legacy entity data into new entity data and push it to prePlacedEntities
 		}
-	}
 
-	foregroundBlockImageUrl(id: number): string {
-		return (
-			this.blockMap.get(id) ??
-			throwExpression(new Error("no block with such id"))
-		)
-	}
+		const mapData: MapData = {
+			width: almostMapData.width,
+			height: almostMapData.height,
+			backgroundBlocks: almostMapData.backgroundBlocks,
+			backgroundImageUrl,
+			blockData: almostMapData.blockData,
+			foregroundBlocks: almostMapData.foregroundBlocks,
+			prePlacedEntities,
+		}
 
-	backgroundBlockImageUrl(id: number): string {
-		return (
-			this.blockMap.get(id) ??
-			throwExpression(new Error("no block with such id"))
-		)
-	}
-
-	loadLegacyEntityDataForName(name: string): ExternalEntityData {
-		throw new Error("Method not implemented.")
+		return {
+			map: mapData,
+		}
 	}
 }
--- app-pk2-web/src/game/resources/res.ts
+++ app-pk2-web/src/game/resources/res.ts
@@ -1,14 +1,31 @@
-import { LegacyEntityData } from "@app/game/entity/external/defines"
-import { BlockId } from "../map"
+import { MapData } from "../map"
+
+/**
+ * Game resources for specific map.
+ * Loaded once per map/game. Map picker/menus are done outside game model.
+ *
+ *
+ * Note: it may contain URLs/lazy load parts.
+ * It's generally everything needed to launch a game, not necessarily run it.
+ *
+ * Some URL preloading may be part of initialization, while doing it.
+ * It should be easy with service worker/gatsby-plugin-offline.
+ * A bit harder for not-gatsby managed URLs, which will be supported in future.
+ */
+export interface GameResources {
+	readonly map: MapData
+}
 
 /**
  * Manager, which manages resources for specified instance of game map.
  */
-export interface ResourceLoader {
-	readonly backgroundColor?: string // color of background, defaults to white
-	readonly backgroundImageUrl?: string // URL of background image, if any
-
-	foregroundBlockImageUrl(id: BlockId): string
-	backgroundBlockImageUrl(id: BlockId): string
-	loadLegacyEntityDataForName(name: string): LegacyEntityData
+export interface LegacyResourceLoader {
+	/**
+	 * Note: This could be sync, but it'd require to embed all/most of assets in js, which is not what I want.
+	 * I want to be able to fetch stuff like map data and entity stuff during this call, while showing user loading screen or sth like that.
+	 */
+	loadLegacyGameResources(
+		epName: string,
+		mapFileName: string,
+	): Promise<GameResources>
 }
