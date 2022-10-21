f0cfa2c0eb1f31a807f705801d7e702b1f32d72b
teawithsand <teawithsand@gmail.com>
2022-10-21 06:39:05 +0000

    Implemented useSubscribable with selector hooks

--- tws-stl-react/src/eventBus/index.ts
+++ tws-stl-react/src/eventBus/index.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState, useCallback } from "react"
-import { Subscribable, StickySubscribable } from "@teawithsand/tws-stl"
+import { StickySubscribable, Subscribable } from "@teawithsand/tws-stl"
+import { useCallback, useEffect, useState } from "react"
 
 export const useSubscribable = <T>(
 	subscribable: Subscribable<T>,
@@ -20,11 +20,31 @@ export const useSubscribable = <T>(
 	return aggregate
 }
 
+export const useSubscribableSelector = <T, E>(
+	subscribable: Subscribable<T>,
+	selector: (data: T) => E,
+	initialValue: E
+): E => {
+	const [aggregate, setAggregate] = useState(() => initialValue)
+
+	useEffect(() => {
+		const releaser = subscribable.addSubscriber((event) => {
+			setAggregate(selector(event))
+		})
+
+		return () => {
+			releaser()
+		}
+	}, [subscribable])
+
+	return aggregate
+}
+
 /**
  * Note: each time callback is changed, listener is re-triggered.
  * This may cause problems with sticky event bus as well as performance problems in general.
  * [useCallback] on callbacks passed here.
- * 
+ *
  * @see useCallback
  */
 export const useSubscribableCallback = <T>(
@@ -50,6 +70,25 @@ export const useStickySubscribable = <T>(
 		setLastEvent(subscribable.lastEvent)
 		const releaser = subscribable.addSubscriber((event) => {
 			setLastEvent(event)
+		})
+
+		return () => {
+			releaser()
+		}
+	}, [subscribable])
+
+	return lastEvent
+}
+
+export const useStickySubscribableSelector = <T, E>(
+	subscribable: StickySubscribable<T>,
+	selector: (data: T) => E
+): E => {
+	const [lastEvent, setLastEvent] = useState(selector(subscribable.lastEvent))
+	useEffect(() => {
+		setLastEvent(selector(subscribable.lastEvent))
+		const releaser = subscribable.addSubscriber((event) => {
+			setLastEvent(selector(event))
 		})
 
 		return () => {
