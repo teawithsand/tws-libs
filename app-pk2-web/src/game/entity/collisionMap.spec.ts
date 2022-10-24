679f50b4f6fae18cc249b1ed7a7ce51855bafa7f
teawithsand <teawithsand@gmail.com>
2022-10-24 12:41:45 +0000

    Implemented util for map collision checking

---
+++ app-pk2-web/src/game/entity/collisionMap.spec.ts
@@ -0,0 +1,55 @@
+import { CollisionsMap } from "@app/game/entity/collisionMap"
+import { BLOCK_HEIGHT, BLOCK_WIDTH } from "@app/game/map"
+import { computeBlockCollisionData } from "@app/game/map/collision"
+import { Rect } from "@app/util/geometry"
+import { transposeMatrix } from "@app/util/matrix"
+
+const rep = <T>(v: T, n: number): T[] => new Array(n).fill(v)
+const sqRep = <T>(v: T, n: number): T[][] => rep(rep(v, n), n)
+
+describe("CollisionMap", () => {
+	it.only("computes proper entity-block collision and surrounding ones", () => {
+		const blocks = transposeMatrix([
+			[-1, -1, -1, -1],
+			[-1, -1, -1, -1], // (32,32) -> (64,64)
+			[-1, -0, -0, -1],
+			[-0, -0, -0, -0],
+		])
+
+		const cm = new CollisionsMap({
+			blockData: {
+				[0]: {
+					backgroundURL: "niy",
+					foregroundURL: "niy",
+					collisions: computeBlockCollisionData(
+						sqRep(true, BLOCK_WIDTH),
+					),
+					isWater: false,
+				},
+			},
+			foregroundBlocks: blocks,
+			backgroundBlocks: blocks,
+		})
+
+		const playerRect = new Rect([
+			[0, 0],
+			[BLOCK_WIDTH, BLOCK_HEIGHT * 2],
+		]).normalized
+
+		// const firstCollisions = cm.queryRectMapCollisions(
+		// 	playerRect.translatedY(BLOCK_HEIGHT * 2),
+		// )
+		//
+		// expect(firstCollisions.isInWater).toStrictEqual(false)
+		// expect(firstCollisions.leftSideCollisions).toStrictEqual(null)
+		// expect(firstCollisions.rightSideCollisions).toStrictEqual(null)
+		
+		const secondCollisions = cm.queryRectMapCollisions(
+			playerRect.translatedY(BLOCK_HEIGHT),
+		)
+
+		expect(secondCollisions.isInWater).toStrictEqual(false)
+		expect(secondCollisions.leftBlocks).toEqual([])
+		expect(secondCollisions.rightBlocks.map(v => v.offsetX)).toEqual([1, 1])
+	})
+})
