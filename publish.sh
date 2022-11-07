c61f7824629c6d27f9d541306aa37253a6a6c9df
teawithsand <teawithsand@gmail.com>
2022-11-07 12:24:35 +0000

    Futher improvements for publish.sh; added tws-ui

--- publish.sh
+++ publish.sh
@@ -1,11 +1,10 @@
 publish() {
-    cd $1 && npm publish --registry http://verdaccio:4873/; cd ..
+    cd $1 && npm run build && npm publish --registry http://verdaccio:4873/; cd ..
 }
-
-npm run build --workspaces
 
 publish "tws-stl";
 publish "tws-stl-react";
 publish "tws-gatsby-plugin";
 publish "tws-player";
 publish "tws-trans";
+publish "tws-ui";
