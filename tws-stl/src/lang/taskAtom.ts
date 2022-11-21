7a3ed4fe678c04d025dd974224371dd4eb406507
teawithsand <teawithsand@gmail.com>
2022-11-21 17:44:36 +0000

    Added addCleaner function for TaskAtomHandle

--- tws-stl/src/lang/taskAtom.ts
+++ tws-stl/src/lang/taskAtom.ts
@@ -1,5 +1,6 @@
 export interface TaskAtomHandle {
 	readonly isValid: boolean
+	addCleaner: (c: () => void) => void
 }
 
 export interface TaskAtom {
@@ -9,19 +10,40 @@ export interface TaskAtom {
 
 export class DefaultTaskAtom implements TaskAtom {
 	private id = -Number.MAX_SAFE_INTEGER
+	private releasers: Array<() => void> = []
 
 	invalidate = (): void => {
 		this.id++
+		this.releasers.forEach((c) => {
+			try {
+				c()
+			} catch (e) {
+				console.error("Error occurred when calling releaser", e)
+			}
+		})
+		this.releasers = []
 	}
 
 	claim = (): TaskAtomHandle => {
 		this.invalidate()
 		const claimId = this.id
 		const idGetter = () => this.id
-
+		const isValid = () => idGetter() === claimId
+		
 		return {
 			get isValid() {
-				return idGetter() === claimId
+				return isValid()
+			},
+			addCleaner: (c: () => void) => {
+				if (!isValid()) {
+					try {
+						c()
+					} catch (e) {
+						console.error("Error occurred when calling releaser", e)
+					}
+				} else {
+					this.releasers.push(c)
+				}
 			},
 		}
 	}
