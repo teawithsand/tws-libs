a6f6990c1dfa658e999c9e4aac715f2106e649d8
teawithsand <teawithsand@gmail.com>
2022-12-07 14:10:16 +0000

    Added new FS api helper tws-stl version bump

---
+++ tws-stl/src/webApi/fullscreen.ts
@@ -0,0 +1,69 @@
+import { DefaultStickyEventBus, StickySubscribable } from "../eventBus"
+
+export enum FullscreenEventType {
+	DISABLED = 0,
+	ENABLED = 1,
+}
+
+export type FullscreenEvent =
+	| {
+			type: FullscreenEventType.ENABLED
+	  }
+	| {
+			type: FullscreenEventType.DISABLED
+	  }
+
+export class FullscreenApiHelper {
+	private readonly innerBus = new DefaultStickyEventBus<FullscreenEvent>({
+		type: this.isFullscreen
+			? FullscreenEventType.ENABLED
+			: FullscreenEventType.DISABLED,
+	})
+
+	get bus(): StickySubscribable<FullscreenEvent> {
+		return this.innerBus
+	}
+
+	private constructor() {
+		if (this.isSupported) {
+			window.addEventListener("fullscreenchange", () => {
+				this.innerBus.emitEvent({
+					type: this.isFullscreen
+						? FullscreenEventType.ENABLED
+						: FullscreenEventType.DISABLED,
+				})
+			})
+		}
+	}
+	private static innerInstance = new FullscreenApiHelper()
+
+	public static get instance() {
+		return this.innerInstance
+	}
+
+	get isSupported() {
+		return (
+			typeof document !== "undefined" &&
+			(typeof document.exitFullscreen !== "undefined" ||
+				typeof document.fullscreenElement !== "undefined")
+		)
+	}
+
+	get isFullscreen() {
+		return this.isSupported && document.fullscreenElement
+	}
+
+	exitFullscreen = async () => {
+		await document.exitFullscreen()
+	}
+
+	/**
+	 * Requests fullscreen either on entire document or specified html element.
+	 */
+	requestFullscreen = async (
+		e: Element = document.documentElement,
+		options?: FullscreenOptions
+	): Promise<void> => {
+		await e.requestFullscreen(options)
+	}
+}
