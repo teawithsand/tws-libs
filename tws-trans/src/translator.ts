11bb4d597c43e087d86deecf8fcd681f0635c6be
teawithsand <teawithsand@gmail.com>
2023-01-03 18:41:41 +0000

    Refactored translator for statically parametrized translations

--- tws-trans/src/translator.ts
+++ tws-trans/src/translator.ts
@@ -1,3 +1,4 @@
+import { throwExpression } from "@teawithsand/tws-stl"
 import { createContext, useContext } from "react"
 import { Language } from "./language"
 import { useLanguage } from "./languageContext"
@@ -10,9 +11,13 @@ import { useLanguage } from "./languageContext"
  */
 export type TranslationObject = {}
 
-export class Translator<T extends TranslationObject> {
+export type TranslationObjectFactory<T extends TranslationObject, P> = (
+	params: P
+) => T
+
+export class Translator<T extends TranslationObject, P> {
 	constructor(
-		private translations: Map<Language, T>,
+		private translations: Map<Language, TranslationObjectFactory<T, P>>,
 		private fallbackLanguage: Language
 	) {
 		if (!translations.has(fallbackLanguage))
@@ -25,18 +30,18 @@ export class Translator<T extends TranslationObject> {
 			)
 	}
 
-	getTranslationForLanguage = (lang?: Language): T => {
-		const res = this.translations.get(lang ?? this.fallbackLanguage)
-		if (!res) return this.translations.get(this.fallbackLanguage) as T
-		return res
+	getTranslationForLanguage = (lang: Language | undefined, params: P): T => {
+		let res = this.translations.get(lang ?? this.fallbackLanguage)
+		if (!res) res = this.translations.get(this.fallbackLanguage)
+		return (res || throwExpression(new Error(`Unreachable code`)))(params)
 	}
 }
 
 /**
  * A context, which has translator generic over type T.
  */
-export type TranslatorContext<T extends TranslationObject> = React.Context<
-	Translator<T>
+export type TranslatorContext<T extends TranslationObject, P> = React.Context<
+	Translator<T, P>
 >
 
 /**
@@ -46,21 +51,25 @@ export type TranslatorContext<T extends TranslationObject> = React.Context<
  * @see TranslatorContext
  * @see createTranslatorContext
  */
-export const makeTranslationHooks = <T extends TranslationObject>(
-	ctx: TranslatorContext<T>
+export const makeTranslationHooks = <T extends TranslationObject, P>(
+	ctx: TranslatorContext<T, P>,
+	paramsLoaderHook: () => P
 ) => ({
-	useTranslator: (): Translator<T> => useContext(ctx),
+	useTranslator: (): Translator<T, P> => useContext(ctx),
 	useTranslation: (): T => {
+		const params = paramsLoaderHook()
 		const lang = useLanguage()
 		const translator = useTranslator(ctx)
 
-		return translator.getTranslationForLanguage(lang)
+		return translator.getTranslationForLanguage(lang, params)
 	},
 	useTranslationSelector: <E>(s: (obj: T) => E): E => {
+		const params = paramsLoaderHook()
+
 		const lang = useLanguage()
 		const translator = useTranslator(ctx)
 
-		return s(translator.getTranslationForLanguage(lang))
+		return s(translator.getTranslationForLanguage(lang, params))
 	},
 })
 
@@ -70,31 +79,35 @@ export const makeTranslationHooks = <T extends TranslationObject>(
  *
  * @see makeTranslationHooks
  */
-export const createTranslatorContext = <T extends TranslationObject>(
-	translator: Translator<T>
-): TranslatorContext<T> => {
-	return createContext<Translator<T>>(translator)
+export const createTranslatorContext = <T extends TranslationObject, P>(
+	translator: Translator<T, P>
+): TranslatorContext<T, P> => {
+	return createContext<Translator<T, P>>(translator)
 }
 
-export const useTranslator = <T extends TranslationObject>(
-	ctx: TranslatorContext<T>
-): Translator<T> => useContext(ctx)
+export const useTranslator = <T extends TranslationObject, P>(
+	ctx: TranslatorContext<T, P>
+): Translator<T, P> => useContext(ctx)
 
-export const useTranslation = <T extends TranslationObject>(
-	ctx: TranslatorContext<T>
+export const useTranslation = <T extends TranslationObject, P>(
+	ctx: TranslatorContext<T, P>,
+	paramsLoaderHook: () => P
 ): T => {
+	const params = paramsLoaderHook()
 	const lang = useLanguage()
 	const translator = useTranslator(ctx)
 
-	return translator.getTranslationForLanguage(lang)
+	return translator.getTranslationForLanguage(lang, params)
 }
 
-export const useTranslationSelector = <T extends TranslationObject, E>(
-	ctx: TranslatorContext<T>,
+export const useTranslationSelector = <T extends TranslationObject, P, E>(
+	ctx: TranslatorContext<T, P>,
+	paramsLoaderHook: () => P,
 	s: (obj: T) => E
 ): E => {
+	const params = paramsLoaderHook()
 	const lang = useLanguage()
 	const translator = useTranslator(ctx)
 
-	return s(translator.getTranslationForLanguage(lang))
+	return s(translator.getTranslationForLanguage(lang, params))
 }
