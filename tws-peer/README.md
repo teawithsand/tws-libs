e8dccc27cd0a973e7c30c4f698824d6dea7ec770
teawithsand <teawithsand@gmail.com>
2023-01-10 16:16:19 +0000

    Initialized some basic tws-peer package

---
+++ tws-peer/README.md
@@ -0,0 +1,6 @@
+# tws-peer
+
+P2P communications via WebRTC done as simply as possible.
+Internally it wraps peer.js, while providing reasonable defaults.
+
+It also adds some more features like automating ID generation and quick setup with special links and/or qr codes.
---
+++ tws-peer/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * As STUN servers are cheap, there is a lot of them available in public.
+ */
+export const STUNServers = [
+    "stun:relay.metered.ca:80",
+	"stun.l.google.com:19302",
+	"stun1.l.google.com:19302",
+	"stun2.l.google.com:19302",
+	"stun3.l.google.com:19302",
+	"stun4.l.google.com:19302",
+]
