e0b8cfa42595dc729ec873787c9541f7a9dc7515
teawithsand <teawithsand@gmail.com>
2023-01-10 18:32:31 +0000

    Added peerjs and some utils

---
+++ tws-peer/src/constants.ts
@@ -0,0 +1,11 @@
+/**
+ * As STUN servers are cheap, there is a lot of them available in public.
+ */
+export const STUNServers = [
+	"stun.l.google.com:19302",
+	"stun1.l.google.com:19302",
+	"stun2.l.google.com:19302",
+	"stun3.l.google.com:19302",
+	"stun4.l.google.com:19302",
+	"stun:relay.metered.ca:80",
+]
--- tws-peer/src/index.ts
+++ tws-peer/src/index.ts
@@ -1,11 +1,3 @@
-/**
- * As STUN servers are cheap, there is a lot of them available in public.
- */
-export const STUNServers = [
-    "stun:relay.metered.ca:80",
-	"stun.l.google.com:19302",
-	"stun1.l.google.com:19302",
-	"stun2.l.google.com:19302",
-	"stun3.l.google.com:19302",
-	"stun4.l.google.com:19302",
-]
+export * from "./constants"
+export * from "./peer"
+export * from "./bus"
