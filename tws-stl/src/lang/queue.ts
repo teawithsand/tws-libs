635d40a9bddccba7ae4e3305be190d3710ce5d56
teawithsand <teawithsand@gmail.com>
2023-01-11 17:00:48 +0000

    Added queue to tws-stl tws-stl version bump

---
+++ tws-stl/src/lang/queue.ts
@@ -0,0 +1,40 @@
+/**
+ * Simple queue, which uses JS's holey arrays in order to provide user with queue.
+ * 
+ * It may be slow on large items count.
+ */
+export class Queue<T> {
+	private inner: T[] = []
+
+	get array(): T[] {
+		return [...this.inner]
+	}
+
+	peek = (): T | null => {
+		if (this.inner.length > 0) return this.inner[0]
+		return null
+	}
+
+	get length() {
+		return this.inner.length
+	}
+
+	get isEmpty() {
+		return this.length === 0
+	}
+
+	clear = () => {
+		this.inner = []
+	}
+
+	push = (v: T) => {
+		this.inner.push(v)
+	}
+
+	pop = (): T | null => {
+		if (this.inner.length === 0) return null
+		const v = this.inner[0]
+		this.inner.splice(0, 1)
+		return v
+	}
+}
