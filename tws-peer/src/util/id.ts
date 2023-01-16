e33a12bcf8286f1a3c32513596472e2159616015
teawithsand <teawithsand@gmail.com>
2023-01-16 09:57:38 +0000

    tws-peer version bump Fixed generateSecureClientId method

--- tws-peer/src/util/id.ts
+++ tws-peer/src/util/id.ts
@@ -7,16 +7,16 @@ export const SECURE_ID_MIN_SIZE = 32
  */
 export const generateSecureClientId = (
 	sizeBytes: number = SECURE_ID_MIN_SIZE
-) => {
+): string => {
 	if (isBrowser) {
 		return [...new Array(sizeBytes).keys()].map(() =>
 			// TODO(teawithsand): optimize this one
 			crypto.getRandomValues(new Uint8Array(1))[0].toString(16)
-		)
+		).join("")
 	} else {
 		return [...new Array(sizeBytes).keys()].map(() =>
 			// TODO(teawithsand): check if this is really uniformly random
 			Math.max(0, Math.round(Math.random() * 256) - 1).toString(16)
-		)
+		).join("")
 	}
 }
