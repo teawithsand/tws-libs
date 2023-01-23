14b1e507a20db29bb8a22dda16dab8181a17af3d
teawithsand <teawithsand@gmail.com>
2023-01-23 19:39:49 +0000

    Refactored *helper to *runHelper in tws-peer

--- tws-peer/src/peerutil/index.ts
+++ tws-peer/src/peerutil/index.ts
@@ -1,5 +1,5 @@
 export * from "./peerBus"
 export * from "./dataConnBus"
 
-export * from "./dataConnHelper"
-export * from "./peerHelper"
+export * from "./dataConnRunHelper"
+export * from "./peerRunHelper"
