00bd021ea1343226e0a7df25e6bf6de971ee2616
teawithsand <teawithsand@gmail.com>
2023-01-26 19:17:30 +0000

    Added peer state tracking feature to peer helper

--- tws-peer/src/peerutil/peerHelper.ts
+++ tws-peer/src/peerutil/peerHelper.ts
@@ -3,10 +3,11 @@ import {
 	DefaultStickyEventBus,
 	StickySubscribable,
 	Subscribable,
+	SubscriptionCanceler,
 } from "@teawithsand/tws-stl"
 import Peer, { PeerJSOption } from "peerjs"
 import { ClosableSubscribable } from "../util"
-import { makePeerBus, PeerEvent } from "./peerBus"
+import { makePeerBus, PeerEvent, PeerEventType } from "./peerBus"
 
 export type PeerHelperConfig = {
 	/**
@@ -19,10 +20,18 @@ export type PeerHelperState = {
 	config: PeerHelperConfig
 	peer: Peer | null
 	bus: ClosableSubscribable<PeerEvent> | null
+
+	peerState: {
+		isClosed: boolean
+		error: Error | null
+	}
 }
 
+// TODO(teawithsand): move and consolidate all state tracking features of peer/data conn in bus helpers,
+//  which should be remodeled and extended for this purpose.
 export class PeerHelper {
 	private readonly innerPeerBus = new DefaultEventBus<PeerEvent>()
+	private innerUnsubscribeFromPeerBus: SubscriptionCanceler | null = null
 
 	/**
 	 * Peer bus in use, if any is available right now.
@@ -38,6 +47,10 @@ export class PeerHelper {
 			},
 			peer: null,
 			bus: null,
+			peerState: {
+				isClosed: true,
+				error: null,
+			},
 		}
 	)
 
@@ -61,14 +74,47 @@ export class PeerHelper {
 		if (prevBus) {
 			prevBus.close()
 		}
+		if (this.innerUnsubscribeFromPeerBus) {
+			this.innerUnsubscribeFromPeerBus()
+			this.innerUnsubscribeFromPeerBus = null
+		}
 		if (peerBus) {
 			peerBus.addSubscriber((e) => this.innerPeerBus.emitEvent(e))
 		}
+
+		this.innerUnsubscribeFromPeerBus =
+			peerBus?.addSubscriber((event) => {
+				const { lastEvent } = this.innerStateBus
+				if (event.type === PeerEventType.ERROR) {
+					this.innerStateBus.emitEvent({
+						...lastEvent,
+						peerState: {
+							...lastEvent.peerState,
+							error: event.error,
+						},
+					})
+				} else if (
+					event.type === PeerEventType.CLOSE ||
+					event.type === PeerEventType.BUS_CLOSE
+				) {
+					this.innerStateBus.emitEvent({
+						...lastEvent,
+						peerState: {
+							...lastEvent.peerState,
+							isClosed: true,
+						},
+					})
+				}
+			}) ?? null
 
 		this.innerStateBus.emitEvent({
 			config: { config },
 			peer: newPeer,
 			bus: peerBus,
+			peerState: {
+				isClosed: false,
+				error: null,
+			},
 		})
 	}
 }
