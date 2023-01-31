e7a09dd7da0e16a4679d0250888be102c0edb5a3
teawithsand <teawithsand@gmail.com>
2023-01-31 21:41:13 +0000

    Added timing-safe-string-eq to tws-peer

--- tws-peer/src/util/id.ts
+++ tws-peer/src/util/id.ts
@@ -3,6 +3,22 @@ const isBrowser = typeof crypto !== "undefined"
 export const SECURE_ID_MIN_SIZE = 32
 
 /**
+ * Compares two strings in time safe manner, if they length is the same.
+ *
+ * !!!IT IS NOT CHECKED, AND ACTUALLY MAY NOT BE CONSTANT-TIME!!!
+ */
+export const timingSafeStringEq = (a: string, b: string): boolean => {
+	if (a.length !== b.length) return false
+
+	let r = 0
+	for (let i = 0; i < a.length; i++) {
+		r |= a.charCodeAt(i) & b.charCodeAt(i)
+	}
+
+	return r === 0
+}
+
+/**
  * Generates secure PeerID for PeerJS. Should not be less than 32 bytes.
  */
 export const generateSecureClientId = (
