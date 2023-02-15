0af121ecc04231ceefe18fa24b88af0a0f7e45ad
teawithsand <teawithsand@gmail.com>
2023-02-15 18:48:20 +0000

    Added username-for-local-registry notice in init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -2,5 +2,7 @@
 
 # NPM has to be installed
 # export SHARP_IGNORE_GLOBAL_LIBVIPS=1
-npm -g install npm
+npm -g install npm@8
 npm set registry http://verdaccio:4873/
+
+# Password for npm login is teawithsand JIC it's needed.
