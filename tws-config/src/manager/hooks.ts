cebc42fb92ff98be6f907b2fbb6bd2589de1797e
teawithsand <teawithsand@gmail.com>
2023-02-22 15:52:00 +0000

    Updated tws-config tws-stl-react version Refactored hooks in tws-config to use new suspense helpers

--- tws-config/src/manager/hooks.ts
+++ tws-config/src/manager/hooks.ts
@@ -1,5 +1,8 @@
 import { throwExpression } from "@teawithsand/tws-stl"
-import { useStickySubscribable } from "@teawithsand/tws-stl-react"
+import {
+	usePromiseSuspense,
+	useStickySubscribable,
+} from "@teawithsand/tws-stl-react"
 import { ConfigManager, ConfigUpdater } from "./manager"
 
 /**
@@ -8,40 +11,25 @@ import { ConfigManager, ConfigUpdater } from "./manager"
  * This hook uses suspense and errorBoundary. Make sure you have provided them in top-level components.
  */
 export const useConfig = <T extends {}>(cm: ConfigManager<T>): T => {
-	let status = 0
-	let error
 	const config = useStickySubscribable(cm.configBus)
-	const promise = useStickySubscribable(cm.loadedPromiseBus).then(
-		() => {
-			status = 1
-		},
-		(e) => {
-			status = -1
-			error = e
-		}
-	)
+	usePromiseSuspense(useStickySubscribable(cm.loadedPromiseBus))
 
-	if (status === 0) {
-		throw promise
-	} else if (status === -1) {
-		throw error
-	} else {
-		return (
-			config ??
-			cm.currentConfig ??
-			throwExpression(
-				new Error(
-					`Unreachable code - config not loaded in suspense even though status is set`
-				)
+	return (
+		config ??
+		cm.currentConfig ??
+		throwExpression(
+			new Error(
+				`Unreachable code - config not loaded in suspense even though status is set`
 			)
 		)
-	}
+	)
 }
 
 /**
  * Fetches config form ConfigManager. If it is not loaded, returns default one.
  */
-export const useConfigOrDefault = <T extends {}>(cm: ConfigManager<T>): T => useStickySubscribable(cm.defaultConfigBus)
+export const useConfigOrDefault = <T extends {}>(cm: ConfigManager<T>): T =>
+	useStickySubscribable(cm.defaultConfigBus)
 
 /**
  * Returns config updater, that is capable of updating configs. In other words it has config loaded.
@@ -51,23 +39,7 @@ export const useConfigOrDefault = <T extends {}>(cm: ConfigManager<T>): T => use
 export const useConfigUpdater = <T extends {}>(
 	cm: ConfigManager<T>
 ): ConfigUpdater<T> => {
-	let status = 0
-	let error
-	const promise = useStickySubscribable(cm.loadedPromiseBus).then(
-		() => {
-			status = 1
-		},
-		(e) => {
-			status = -1
-			error = e
-		}
-	)
+	usePromiseSuspense(useStickySubscribable(cm.loadedPromiseBus))
 
-	if (status === 0) {
-		throw promise
-	} else if (status === -1) {
-		throw error
-	} else {
-		return cm
-	}
+	return cm
 }
