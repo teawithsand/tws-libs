9aa05fea173406459e897dae460d65a427356a95
teawithsand <teawithsand@gmail.com>
2023-03-25 14:46:08 +0000

    Implemented TransformAndValidateSerializer

--- tws-config/src/serialization/serializer.ts
+++ tws-config/src/serialization/serializer.ts
@@ -1,3 +1,10 @@
+import { ClassTransformOptions, instanceToPlain } from "class-transformer"
+import {
+	ClassType,
+	transformAndValidateSync,
+	TransformValidationOptions,
+} from "class-transformer-validator"
+
 export interface Serializer<T, E> {
 	serialize: (value: T) => E
 	deserialize: (value: E) => T
@@ -9,4 +16,38 @@ export interface Serializer<T, E> {
 export class IdentitySerializer<T> implements Serializer<T, T> {
 	serialize = (value: T) => value
 	deserialize = (value: T) => value
+}
+
+export class TransformAndValidateSerializer<T extends object>
+	implements Serializer<T, Record<string, any>>
+{
+	constructor(
+		public readonly clazz: ClassType<T>,
+		public readonly serializationOptions?: Readonly<ClassTransformOptions>,
+		public readonly deserializationOptions?: Readonly<TransformValidationOptions>
+	) {}
+	serialize = (value: T) => {
+		if (!(value instanceof this.clazz)) {
+			throw new Error(
+				`Serialization filed - invalid value type provided; got ${value} expected ${this.clazz}`
+			)
+		}
+		return instanceToPlain(value, this.serializationOptions)
+	}
+
+	deserialize = (value: Record<any, string> | string) => {
+		const res = transformAndValidateSync<T>(
+			this.clazz,
+			JSON.stringify(value),
+			this.deserializationOptions
+		)
+
+		if (!(res instanceof this.clazz)) {
+			throw new Error(
+				`Deserialization filed - invalid result type got ${res} expected ${this.clazz}`
+			)
+		}
+
+		return res
+	}
 }
